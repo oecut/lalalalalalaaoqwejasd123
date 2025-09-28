61af9c24a8d358253ee7b9aa6e72ae8ddca6e701
oecut <tronsmart054@gmail.com>
2025-09-28 12:23:35 +0300

    Delete lalalalalalaaoqwejasd123/lalalalalalaaoqwejasd123 directory

--- lalalalalalaaoqwejasd123/lalalalalalaaoqwejasd123/ErrrorBot1/keys.py
+++
@@ -1,8 +0,0 @@
-# /ErrrorBot/keys.py
-
-# Токен вашего бота от @BotFather
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
-
-# Список числовых Telegram ID администраторов бота
-# Пример: ADMIN_IDS = [123456789] или ADMIN_IDS = [123456789, 987654321]
-ADMIN_IDS = [123456789]
