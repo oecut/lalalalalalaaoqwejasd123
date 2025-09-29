b10389d9722b10c606c7aa866b57fdd385eb8ec7
oecut <tronsmart054@gmail.com>
2025-09-29 21:50:51 +0300

    Delete ErrrorBot1(4) directory

--- ErrrorBot1(4)/keys - Копия.py
+++
@@ -1,8 +0,0 @@
-# /ErrrorBot/keys.py
-
-# Токен вашего бота от @BotFather
-BOT_TOKEN = "токен"
-
-# Список числовых Telegram ID администраторов бота
-# Пример: ADMIN_IDS = [123456789] или ADMIN_IDS = [123456789, 987654321]
-ADMIN_IDS = [1234]
